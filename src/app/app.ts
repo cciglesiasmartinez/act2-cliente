4f6e4fd7afe8b6948b096bd54af128361172efb6
Casandra Carolina Iglesias Martínez <cciglesiasmartinez@gmail.com>
2025-12-15 22:06:27 +0100

    FEAT: Adds ProductForm and ProductList components no App main component.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductService, Product } from './services/product';
+import { ProductList } from './components/product-list/product-list';
+import { ProductFormComponent } from './components/product-form/product-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProductList, ProductFormComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('actividad2');
+
+  constructor(private productService: ProductService) {
+    this.productService.getProducts().subscribe(
+      (datos: Product[]) => {
+        console.log('Datos recibidos del servicio:', datos);
+      }
+    );
+  }
+
+  onProductCreated(newProduct: Product) {
+    console.log('Nuevo producto creado:', newProduct);
+    // Aquí podrías agregar lógica para añadir el nuevo producto a la lista existente
+  }
+
 }
